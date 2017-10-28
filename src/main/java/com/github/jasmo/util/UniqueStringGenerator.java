42a2dffa6f256713319220fde69ba129c64c4833
Caleb Whiting <caleb.andrew.whiting@gmail.com>
2017-10-28 12:47:26 +0100

    Simplified the default unique string generator

--- src/main/java/com/github/jasmo/util/UniqueStringGenerator.java
+++ src/main/java/com/github/jasmo/util/UniqueStringGenerator.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * @author Caleb Whiting
@@ -16,7 +14,9 @@ public interface UniqueStringGenerator {
 
 	String next();
 
-
+	/**
+	 * Unpredictable and made up of characters outside of the 'normal' range
+	 */
 	class Crazy implements UniqueStringGenerator {
 
 		private static final Random rng = new Random();
@@ -44,90 +44,32 @@ public interface UniqueStringGenerator {
 		}
 	}
 
+	/**
+	 * Increments similar to a number
+	 * Last number that is < Z is incremented, all values after the incremented value are reset to 'A'.
+	 * If all values are Z then increase length
+	 */
 	class Default implements UniqueStringGenerator {
 
-		private static final String FIRST = "a";
+		char[] chars = new char[0];
 
-		private static String text = "";
-
-		private static void next(StringBuilder buf, int pos, boolean alphaNum) {
-			if (pos == -1) {
-				char c = buf.charAt(0);
-				String rep;
-				if (Character.isDigit(c))
-					rep = "1";
-				else if (Character.isLowerCase(c))
-					rep = "a";
-				else if (Character.isUpperCase(c))
-					rep = "A";
-				else
-					rep = Character.toString((char) (c + 1));
-				buf.insert(0, rep);
-				return;
-			}
-			char c = buf.charAt(pos);
-			if (Character.isDigit(c)) {
-				if (c == '9') {
-					buf.replace(pos, pos + 1, "0");
-					next(buf, pos - 1, alphaNum);
-				} else {
-					buf.replace(pos, pos + 1, Character.toString((char) (c + 1)));
-				}
-			} else if (Character.isLowerCase(c)) {
-				if (c == 'z') {
-					buf.replace(pos, pos + 1, "a");
-					next(buf, pos - 1, alphaNum);
-				} else {
-					buf.replace(pos, pos + 1, Character.toString((char) (c + 1)));
-				}
-			} else if (Character.isUpperCase(c)) {
-				if (c == 'Z') {
-					buf.replace(pos, pos + 1, "A");
-					next(buf, pos - 1, alphaNum);
-				} else {
-					buf.replace(pos, pos + 1, Character.toString((char) (c + 1)));
-				}
-			} else {
-				if (alphaNum) {
-					next(buf, pos - 1, true);
-				} else {
-					if (c == Character.MAX_VALUE) {
-						buf.replace(pos, pos + 1, Character.toString(Character.MIN_VALUE));
-						next(buf, pos - 1, false);
-					} else {
-						buf.replace(pos, pos + 1, Character.toString((char) (c + 1)));
-					}
-				}
-			}
-		}
-
+		@Override
 		public void reset() {
-			text = FIRST;
+			chars = new char[0];
 		}
 
+		@Override
 		public String next() {
-			int len = text.length();
-			if (len == 0)
-				return text = FIRST;
-			boolean alphaNum = false;
-			int alphaNumPos = -1;
-			for (char c : text.toCharArray()) {
-				alphaNumPos++;
-				if (Character.isDigit(c) || Character.isLetter(c)) {
-					alphaNum = true;
-					break;
+			for (int n = chars.length - 1; n >= 0; n--) {
+				if (chars[n] < 'Z') {
+					chars[n]++;
+					return new String(chars);
 				}
+				chars[n] = 'A';
 			}
-			StringBuilder buf = new StringBuilder(text);
-			if (!alphaNum || alphaNumPos == 0 || alphaNumPos == len) {
-				next(buf, buf.length() - 1, alphaNum);
-			} else {
-				String prefix = text.substring(0, alphaNumPos);
-				buf = new StringBuilder(text.substring(alphaNumPos));
-				next(buf, buf.length() - 1, true);
-				buf.insert(0, prefix);
-			}
-			return text = buf.toString();
+			chars = new char[chars.length + 1];
+			Arrays.fill(chars, 'A');
+			return new String(chars);
 		}
 
 	}
